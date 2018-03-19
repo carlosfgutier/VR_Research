2c2d0ec1b3eb5f85a40044a80d28752471807afe
Carlos <carlosfgutier@gmail.com>
2018-03-19 16:43:39 -0500

    playing with cards layout

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,13 +1,13 @@
 $('#hypothesis').on('click', function() {
-	$('#cardInfoSection').html('<div id="cardInfo"><p>Three Acts of Kindness is a positive psychology intervention with extensive scientific evidence in support of its ability to increase happiness.<br>The intervention consists of engaging in three kind acts, all in one day, and recording them.<br> So we asked ourselves, will performing three kind acts in a virtual environment achieve similar results?<br>The answer is yes.</p></div>');
+	$('#cardInfoSection').html('<div id="cardInfo"><h3>Hypothesis</h3><p>Three Acts of Kindness is a positive psychology intervention with extensive scientific evidence in support of its ability to increase happiness.<br>The intervention consists of engaging in three kind acts, all in one day, and recording them.<br> So we asked ourselves, will performing three kind acts in a virtual environment achieve similar results?<br>The answer is yes.</p></div>');
 });
 
 $('#sample').on('click', function() {
-	$('#cardInfoSection').html('<div id="cardInfo"><p>31 participants where randomly selected and randomly assigned to one of two groups, Kind Acts (experimental group) or Mundane Acts (control).<br>Participants in the Kind Act group saw a virtual version of themselves reading a story to a baby, feeding a baby, and playing with a baby.<br>Participants in the Mundane Acts group saw a virtual version of themselves brushing their teeth, watching TV, and drinking coffee.</p></div>');
+	$('#cardInfoSection').html('<div id="cardInfo"><h3>Sample</h3><p>31 participants where randomly selected and randomly assigned to one of two groups, Kind Acts (experimental group) or Mundane Acts (control).<br>Participants in the Kind Act group saw a virtual version of themselves reading a story to a baby, feeding a baby, and playing with a baby.<br>Participants in the Mundane Acts group saw a virtual version of themselves brushing their teeth, watching TV, and drinking coffee.</p></div>');
 });
 
 $('#methods').on('click', function() {
-	$('#cardInfoSection').html('<div id="cardInfo"><p>Pre-test and post-test scores of depression and happiness where collected through the Center for Epidemiology Studies Depression Scale (CESD-R) and the Steen Happiness Index (SHI), respectively.<br>Virtual environments where created and recorded using The Sims 4.<br>A Google Pixel phone and a Google Cardboard served as the Virtual Reality hardware.<br>A Mixed Models ANOVA allowed us to compare the pre-test and post-test results for each group as well as the differences between the two groups.</p></div>');
+	$('#cardInfoSection').html('<div id="cardInfo"><h3>Methods</h3><p>Pre-test and post-test scores of depression and happiness where collected through the Center for Epidemiology Studies Depression Scale (CESD-R) and the Steen Happiness Index (SHI), respectively.<br>Virtual environments where created and recorded using The Sims 4.<br>A Google Pixel phone and a Google Cardboard served as the Virtual Reality hardware.<br>A Mixed Models ANOVA allowed us to compare the pre-test and post-test results for each group as well as the differences between the two groups.</p></div>');
 });
 
 
